ddec275e247cd6da554333f79752573a93244620
Dawid <praca.dawidwk@gmail.com>
2021-12-12 13:28:16 +0100

    added secret_key env variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET")
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET",
+    "django-insecure-fsagoj%o#_5gl%iem(w55%311c=$$+(7vj+09f2+cvs)6=-*7eu",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
