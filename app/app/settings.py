17438a1ac89e1d0859db75eddd3f7557a311454c
Dawid <praca.dawidwk@gmail.com>
2021-12-12 13:21:31 +0100

    added secret_key env variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,15 +75,6 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("DB_HOST"),
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-    }
-}
 
 # Database settings for github actions
 if os.environ.get("GITHUB_WORKFLOW"):
@@ -95,6 +86,16 @@ if os.environ.get("GITHUB_WORKFLOW"):
             "PASSWORD": "postgres",
             "HOST": "127.0.0.1",
             "PORT": "5432",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": os.environ.get("DB_HOST"),
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
         }
     }
 
